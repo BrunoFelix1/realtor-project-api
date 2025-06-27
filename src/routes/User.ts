451c52bc052497f8938ea0c08e2a60325310012f
Bruno Felix <brunofelixpinto@gmail.com>
2025-06-27 14:31:30 -0300

    fix: envolving auth middleware at users reqs and adding jwt to configs example

--- src/routes/User.ts
+++ src/routes/User.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import UserController from '../controllers/UserController';
+import { authenticateToken } from '../middleware/auth';
 
 const router = Router();
+
+router.use(authenticateToken);
 
 // Listar usuários
 router.get('/', UserController.list);
